bdf7a8e1dc14e9c2d113bfa8246ceadc7ca4953b
Auros Nexus <aurosnex@gmail.com>
2020-09-09 22:46:51 -0400

    Light config

--- server/Startup.cs
+++ server/Startup.cs
@@ -10,7 +10,6 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-
 namespace ModelSaber
 {
     public class Startup
@@ -25,6 +24,7 @@ namespace ModelSaber
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+
             services.AddControllers();
         }
 
