0ba1d7b05c596f88fadd8175f9a77ef978d6d156
Auros Nexus <aurosnex@gmail.com>
2020-09-16 18:06:49 -0400

    Finished user model, probably

---
+++ ModelSaber.Common/Role.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ModelSaber.Common
+{
+    [Flags]
+    public enum Role
+    {
+        None = 0,
+        Uploader = 1,
+        Verified = 2,
+        Trusted = 4,
+        Manager = 8,
+        Admin = 16
+    }
+}
--- ModelSaber.Common/User.cs
+++ ModelSaber.Common/User.cs
@@ -10,10 +10,18 @@ namespace ModelSaber.Common
         public Guid Id { get; set; }
 
         /// <summary>
+        /// The role of the user in the context of ModelSaber
+        /// </summary>
+        public Role Role { get; set; }
+
+        /// <summary>
+        /// The Markdown description (or bio) of the user.
+        /// </summary>
+        public string Biography { get; set; }
+
+        /// <summary>
         /// The discord profile of the user.
         /// </summary>
         public DiscordUser Profile { get; set; }
-
-
     }
 }
