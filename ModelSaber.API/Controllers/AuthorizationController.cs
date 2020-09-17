e0f85d240fa25dbc80c5c4f24dff1a1a6f57547e
Auros Nexus <aurosnex@gmail.com>
2020-09-16 23:54:26 -0400

    Create AuthorizationController.cs

---
+++ ModelSaber.API/Controllers/AuthorizationController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ModelSaber.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthorizationController : ControllerBase
+    {
+        private readonly ModelSaberContext _modelSaberContext;
+
+        public AuthorizationController(ModelSaberContext modelSaberContext)
+        {
+            _modelSaberContext = modelSaberContext;
+        }
+    }
+}
