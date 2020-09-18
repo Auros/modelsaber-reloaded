7c257a067454dd6f35951dbc68c386f3eba92d67
Auros Nexus <aurosnex@gmail.com>
2020-09-18 16:26:36 -0400

    Votes done

--- ModelSaber.API/Controllers/VoteController.cs
+++ ModelSaber.API/Controllers/VoteController.cs
@@ -1,14 +1,18 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
-using System;
-using System.Collections.Generic;
+﻿using System;
 using System.Linq;
+using ModelSaber.Common;
+using ModelSaber.API.Models;
 using System.Threading.Tasks;
+using ModelSaber.API.Security;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
 
 namespace ModelSaber.API.Controllers
 {
     [ApiController]
-    [Route("api/[controller]s")]
+    [Route("api/[controller]")]
     public class VoteController : ControllerBase
     {
         private readonly ModelSaberContext _modelSaberContext;
@@ -18,5 +22,66 @@ namespace ModelSaber.API.Controllers
             _modelSaberContext = modelSaberContext;
         }
 
+        [Maulth]
+        [HttpPost("{source}")]
+        public async Task<IActionResult> Vote(Guid source, [FromQuery(Name = "upvote")] bool isUpvote)
+        {
+            if (!await ItemExistsInDatabase(source))
+            {
+                return NotFound();
+            }
+            User voter = (User)HttpContext.Items["User"];
+            Vote vote = await _modelSaberContext.Votes.FirstOrDefaultAsync(v => v.Source == source && v.Voter.Id == voter.Id);
+            if (vote == null)
+            {
+                vote = (await _modelSaberContext.Votes.AddAsync(new Vote { Source = source, Voter = voter, IsUpvote = isUpvote })).Entity;
+                await _modelSaberContext.SaveChangesAsync();
+            }
+            else
+            {
+                if (isUpvote == vote.IsUpvote)
+                {
+                    _modelSaberContext.Votes.Remove(vote);
+                    await _modelSaberContext.SaveChangesAsync();
+                    return NoContent();
+                }
+                else
+                {
+                    vote.IsUpvote = !isUpvote;
+                    await _modelSaberContext.SaveChangesAsync();
+                }
+            }
+            return Ok(vote);
+        }
+
+        [HttpGet("{source}")]
+        public IActionResult GetVotes(Guid source)
+        {
+            IEnumerable<Vote> votes = _modelSaberContext.Votes.Where(v => v.Source == source);
+            return Ok(new VoteStats { Upvotes = votes.Where(v => v.IsUpvote).Count(), Downvotes = votes.Where(v => !v.IsUpvote).Count() });
+        }
+
+        [HttpGet("@me/{source}")]
+        public async Task<IActionResult> GetVoteFromSelf(Guid source)
+        {
+            User voter = (User)HttpContext.Items["User"];
+            Vote vote = await _modelSaberContext.Votes.FirstOrDefaultAsync(v => v.Source == source && v.Voter.Id == voter.Id);
+            if (vote == null)
+            {
+                return NotFound();
+            }
+            return Ok(vote);
+        }
+
+        public async Task<bool> ItemExistsInDatabase(Guid id)
+        {
+            Type[] types = new Type[] { typeof(User), typeof(Model), typeof(Comment), typeof(Playlist) };
+            for (int i = 0; i < types.Length; i++)
+            {
+                object obj = await _modelSaberContext.FindAsync(types[i], id);
+                if (obj != null) return true;
+            }
+            return false;
+        }
     }
 }
---
+++ ModelSaber.API/Models/VoteStats.cs
@@ -0,0 +1,8 @@
+﻿namespace ModelSaber.API.Models
+{
+    public class VoteStats
+    {
+        public int Upvotes { get; set; }
+        public int Downvotes { get; set; }
+    }
+}
