645f15221c91cae45979ac79041e57b552afc313
Auros Nexus <aurosnex@gmail.com>
2020-10-08 23:25:09 -0400

    space cleanup

--- ModelSaber.Web/pages/_app.tsx
+++ ModelSaber.Web/pages/_app.tsx
@@ -1,12 +1,12 @@
-import { AppProps, Container } from 'next/app'
+import { AppProps } from 'next/app'
 import { AuthProvider } from '../components/store'
 
 function App({ Component, pageProps }: AppProps) {
-  return (
-    <AuthProvider>
-      <Component {...pageProps} />
-    </AuthProvider>
-  )
+    return (
+        <AuthProvider>
+            <Component {...pageProps} />
+        </AuthProvider>
+    )
 }
 
 export default App
--- ModelSaber.Web/pages/index.tsx
+++ ModelSaber.Web/pages/index.tsx
@@ -2,22 +2,22 @@ import Head from 'next/head'
 import { useAuth, useDispatchAuth } from '../components/store'
 
 const Index = () => {
-  const auth = useAuth()
-  const dispatch = useDispatchAuth()
+    const auth = useAuth()
+    const dispatch = useDispatchAuth()
 
-  const handleLogin = (_event: any) => dispatch({ type: 'LOGGED_IN', payload: 'etf.my_token.xyz' })
-  const handleLogout = (_event: any) => dispatch({ type: 'LOG_OUT' })
+    const handleLogin = (_event: any) => dispatch({ type: 'LOGGED_IN', payload: 'etf.my_token.xyz' })
+    const handleLogout = (_event: any) => dispatch({ type: 'LOG_OUT' })
 
-  return (
-    <>
-      <Head>
-        <title>ModelSaber</title>
-      </Head>
-      <p>Token: {auth.token}</p>
-      <button onClick={handleLogin}>Log In</button>
-      <button onClick={handleLogout}>Log Out</button>
-    </>
-  )
+    return (
+        <>
+            <Head>
+                <title>ModelSaber</title>
+            </Head>
+            <p>Token: {auth.token}</p>
+            <button onClick={handleLogin}>Log In</button>
+            <button onClick={handleLogout}>Log Out</button>
+        </>
+    )
 }
 
 export default Index
